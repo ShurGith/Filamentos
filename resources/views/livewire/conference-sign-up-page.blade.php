3ec9f77886997796fc3144fd8078c9bc665210f8
ShurGith <75266693+ShurGith@users.noreply.github.com>
2024-12-06 18:05:36 +0100

    enlace home

--- resources/views/livewire/conference-sign-up-page.blade.php
+++ resources/views/livewire/conference-sign-up-page.blade.php
@@ -30,7 +30,7 @@
         <h1 class="mt-10 text-5xl font-semibold tracking-tight text-white text-pretty sm:text-7xl">Deploy to the cloud with confidence</h1>
         <p class="mt-8 text-lg font-medium text-gray-400 text-pretty sm:text-xl/8">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.</p>
         <div class="flex items-center mt-10 gap-x-6">
-          {{ $this->signUpAction }}
+          {{ $this->signUpAction }} <a style="--c-400:var(--primary-400);--c-500:var(--primary-500);--c-600:var(--primary-600);" class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50 fi-ac-action fi-ac-btn-action" href="{{ route('home') }}">Home</a>
         </div>
       </div>
       <div class="flex max-w-2xl mx-auto mt-16 sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use App\Livewire\ConferenceSignUpPage;
 
 Route::get('/', function () {
    return view('welcome');
-});
+})->name('home');
 Route::get('/conference-signup', ConferenceSignUpPage::class)->name('conference-signup');
